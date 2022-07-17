c9a3356cc08dfffccbefe97f0a7009367eb363a0
AnaviaD <anavia_n@outlook.com>
2022-07-16 22:44:28 -0500

    Adding the html and JS File

---
+++ matrix.js
@@ -0,0 +1,8 @@
+document.body.style.background = "black"
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+
+ let cw = window.innerWidth
+ let ch = window.innerHeight
+
+ 
